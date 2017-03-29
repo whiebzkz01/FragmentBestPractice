e88ed4ad4a9889515b43ad7c1808b54919491835
whiebzkz <kongzhenzhu@foxmail.com>
2017-03-29 20:31:17 +0800

    Initial commit

---
+++ app/src/main/java/com/whieb/fragmentbestpractice/NewsContentActivity.java
@@ -0,0 +1,27 @@
+package com.whieb.fragmentbestpractice;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_content);
+
+        String newsTitle = getIntent().getStringExtra("news_title");
+        String newsContent = getIntent().getStringExtra("news_content");
+        NewsContentFragment newsContentFragment = (NewsContentFragment) getSupportFragmentManager().findFragmentById(R.id.news_content_fragment);
+        newsContentFragment.refresh(newsTitle, newsContent);
+    }
+
+    public static void actionStart(Context context, String newsTitle, String newsContent){
+        Intent intent = new Intent(context, NewsContentActivity.class);
+        intent.putExtra("newsTitle", newsTitle);
+        intent.putExtra("newsContent", newsContent);
+        context.startActivity(intent);
+    }
+}
